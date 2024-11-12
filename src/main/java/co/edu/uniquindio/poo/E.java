bb0162822618f4257c4a2167adc862a78e2e688f
usuario <moisesa.vargasr@uqvirtual.edu.co>
2024-11-11 23:00:54 -0500

    guardar

---
+++ src/main/java/co/edu/uniquindio/poo/E.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.poo;
+
+public class E {
+    public String nombre;
+
+    public E(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
